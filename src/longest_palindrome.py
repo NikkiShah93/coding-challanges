6980936e064f0b43264a16fd1ede6049794641de
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-10-09 16:59:08 -0400

    find the longest palindrome

---
+++ src/longest_palindrome.py
@@ -0,0 +1,28 @@
+## the gaol is find the longest palindrome
+
+import os
+## custom module for loading
+from load_data import load_data
+CWD = os.getcwd()
+DATA_PATH = f"{CWD}/data"
+FILE_NAME = 'sowpods.txt'
+SCORES = 'scores.json'
+
+def longest_palindrome(**kwargs):
+    DATA_PATH, FILE_NAME, SCORES = kwargs['DATA_PATH'], kwargs['FILE_NAME'], kwargs['SCORES']
+    data, _ = load_data(DATA_PATH, FILE_NAME, SCORES)
+    longest_word = ''
+    for word in data:
+        if word == word[::-1]:
+            if len(word) > len(longest_word):
+                longest_word = word
+    return longest_word
+
+
+kwargs = dict(
+    DATA_PATH=DATA_PATH,
+    FILE_NAME=FILE_NAME,
+    SCORES=SCORES
+)
+
+print(longest_palindrome(**kwargs))
