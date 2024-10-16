7d3a6fb0fc053b26283aebd89d45dab4c0fcb2c4
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-10-16 15:35:40 -0400

    finding the most frequent letters

---
+++ src/letter_freq.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+CWD = os.getcwd()
+FILE_NAME = 'constitution.txt'
+FILE_PATH = f'{os.getcwd()}\\data\\{FILE_NAME}'
+
+## reading the content
+# content = open(FILE_PATH).readlines()
+
+## generating the letter dict
+letter_dict = {}
+with open(FILE_PATH) as f:
+    for line in f:
+        letters = set(re.sub('[^a-z]+', '',line.strip().lower()))
+        for l in letters:
+            if l in letter_dict:
+                letter_dict[l] += 1
+            else:
+                letter_dict[l] = 1
+
+## picking the top five
+top_five = [(l, c) for l, c in sorted(list(letter_dict.items()),key=lambda x: x[1])[-5:]][::-1]
+print(letter_dict)
+print(top_five)
