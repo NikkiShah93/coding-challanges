23cecbd69d1e8b543680dae0218093aa3ad21f03
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-10-21 17:48:18 -0400

    made it more verbose

--- src/scaping.py
+++ src/scaping.py
@@ -6,9 +6,22 @@ import urllib.request as request
 from bs4 import BeautifulSoup
 import json
 
+CWD = os.getcwd()
+DATA_PATH = f"{CWD}/data/img/"
+
 response = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=10").read()
 content = json.loads(response)
 
 for row in content:
-    print(row.keys())
-    print()
+    if "url" in row:
+        img_url = row.get("url")
+        print(f"working on {img_url}")
+        image_content = request.urlopen(img_url).read()
+        title = row.get('title', "no_title").strip().replace(" ", "_")
+        img_type = img_url.split('.')[-1] if "." in img_url else ".jpg"
+        ## writing the binary to disk
+        with open(f"{DATA_PATH}{title}.{img_type}", 'wb') as img:
+            img.write(image_content)
+            print(f"{DATA_PATH}{title}.{img_type} is written!")
+        
+        
