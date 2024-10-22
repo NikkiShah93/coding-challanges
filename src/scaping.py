45bf93e4720b835d72c1274a3c7592acaf8e5edf
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-10-22 17:04:34 -0400

    made it platform agnostic

--- src/scaping.py
+++ src/scaping.py
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 import json
 
 CWD = os.getcwd()
-DATA_PATH = f"{CWD}/data/img/"
+DATA_PATH = os.path.join(CWD, "data")
+IMG_PATH = os.path.join(DATA_PATH, "img")
 
 response = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=10").read()
 content = json.loads(response)
@@ -18,10 +19,10 @@ for row in content:
         print(f"working on {img_url}")
         image_content = request.urlopen(img_url).read()
         title = row.get('title', "no_title").strip().replace(" ", "_")
-        img_type = img_url.split('.')[-1] if "." in img_url else ".jpg"
+        img_name = img_url.split('/')[-1] if "." in img_url else ".jpg"
         ## writing the binary to disk
-        with open(f"{DATA_PATH}{title}.{img_type}", 'wb') as img:
+        with open(f"{os.path.join(IMG_PATH,img_name)}", 'wb') as img:
             img.write(image_content)
-            print(f"{DATA_PATH}{title}.{img_type} is written!")
+            print(f"{img_name} is written!")
         
         
